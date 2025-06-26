3479d8e8ad91d4134889956bf2646090d5fccd5d
Victor Zawacki Monteiro <victorzawacki9@gmail.com>
2025-06-26 11:40:47 -0300

    Commit criação de filmes e sessões.

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,21 @@ public class Main {
         Pessoa pessoa = new Pessoa();
 
         // FILMES
-        Filme filme1 = new Filme("Creed", 120, "Ação", "+18");
+        Filme filme1 = new Filme("Creed III", 116, "Drama", "+12");
+        Filme filme2 = new Filme("Pecadores", 137, "Ação, Terror", "+18");
+        Filme filme3 = new Filme("Eu sou a lenda", 100, " Ficção Científica", "+16");
+
+        // SESSÕES
+        Sessao sessaoC1 = new Sessao(1,"18:00", "Inglês", 30, filme1);
+        Sessao sessaoC2 = new Sessao(2,"20:30", "Português", 30, filme1);
+
+        Sessao sessaoP1 = new Sessao(2,"18:00", "Inglês", 30, filme2);
+        Sessao sessaoP2 = new Sessao(3,"22:00", "Português", 30, filme2);
+
+        Sessao sessaoL1 = new Sessao(3,"19:00", "Português", 30, filme3);
+        Sessao sessaoL2 = new Sessao(1,"21:30", "Inglês", 30, filme3);
+
+        System.out.println(sessaoC1.getNomeDoFilme());
 
         while (true){
             // MENU
@@ -20,6 +34,7 @@ public class Main {
             System.out.println("3-Filmes: ");
             System.out.println("4-Bombo: ");
             System.out.println("5-Pagar: ");
+            System.out.println("6-Admin: ");
             System.out.println("0-Sair: ");
 
             System.out.println("Escolha uma opção: ");
@@ -43,12 +58,23 @@ public class Main {
                     senhaTmp = scanner.nextLine();
                     if (pessoa.verificarLogin(nomeTmp, senhaTmp)) {
                         System.out.println("Login realizado com sucesso!");
-                        pessoa.setLogado(true);
                     } else {
                         System.out.println("Usuário e senha inválidos!");
                     }
                     break;
                 case 3:
+                    pessoa.verificarAutenticacao();
+                    break;
+                case 4:
+                    pessoa.verificarAutenticacao();
+                    break;
+                case 5:
+                    pessoa.verificarAutenticacao();
+                    break;
+                case 6:
+                    pessoa.verificarAutenticacao();
+                    break;
+                case 0:
                     pessoa.verificarAutenticacao();
                     break;
             }
--- src/Sessao.java
+++ src/Sessao.java
@@ -3,4 +3,17 @@ public class Sessao{
     public String horario;
     public String idioma;
     public int preco;
+    Filme nomeFilme;
+
+    Sessao(int sala, String horario, String idioma, int preco, Filme nomeFilme){
+        this.sala = sala;
+        this.horario = horario;
+        this.idioma = idioma;
+        this.preco = preco;
+        this.nomeFilme = nomeFilme;
+    }
+
+    public String getNomeDoFilme() {
+        return nomeFilme.getTitulo();
+    }
 }
